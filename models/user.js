c4cd44a5052db843ec48271ca8c3f7e27bf0b233
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-26 23:48:26 +0300

    add start model and schema for user

---
+++ models/user.js
@@ -0,0 +1,73 @@
+const { Schema, model } = require('mongoose');
+const Joi = require('joi');
+
+const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      match: emailRegexp,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      minlength: 6,
+      required: true,
+    },
+    token: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    avatarUrl: {
+      type: String,
+      required: true,
+    },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, 'Verify token is required'],
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const registerSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const userEmailSchema = Joi.object({
+  email: Joi.string()
+    .pattern(emailRegexp)
+    .required()
+    .messages({ 'any.required': 'Missing required field email' }),
+});
+
+const loginSchema = Joi.object({
+  email: Joi.string().pattern(emailRegexp).required(),
+  password: Joi.string().min(6).required(),
+});
+
+const schemas = {
+  registerSchema,
+  userEmailSchema,
+  loginSchema,
+};
+
+const User = model('user', userSchema);
+
+module.exports = {
+  User,
+  schemas,
+};
