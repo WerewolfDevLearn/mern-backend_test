6930757fcc2c1e7a80a8d49ef5c1b64cdce04aac
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-27 11:24:44 +0300

    add auth router, add middleware for static files

--- app.js
+++ app.js
@@ -6,12 +6,18 @@ require('dotenv').config();
 const app = express(); // web server
 
 // add router from folder routes
+const authRouter = require('./routes/api/auth-routes');
 
 const formatLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatLogger));
 app.use(cors({ origin: '*' }));
 app.use(express.json());
+
+// add static
+app.use(express.static('public'));
+
+app.use('/api/auth', authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not Found' });
