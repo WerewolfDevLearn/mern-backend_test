dcc95bd7b74db987c4c307da79ebfd967330e7c7
Mikhail Khodak <werewolfdevlearn@gmail.com>
2023-08-01 22:09:12 +0300

    lint fix

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -6,6 +6,7 @@ const updateAvatar = ctrlWrapper(async (req, res) => {
   const { avatarId } = req.user;
   if (avatarId) await cloudinary.destroy(avatarId);
 
+  // eslint-disable-next-line camelcase
   const { url, public_id } = await cloudinary.upload(req.file.path);
   const avatar = { avatarUrl: url, avatarId: public_id };
   const newUser = await User.findByIdAndUpdate(req.user._id, avatar);
