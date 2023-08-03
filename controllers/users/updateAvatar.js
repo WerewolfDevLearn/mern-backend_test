9f907559b37020c1a723f8d21782642885f5226c
Mikhail Khodak <werewolfdevlearn@gmail.com>
2023-08-03 15:15:54 +0300

    multer test

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,18 +1,19 @@
-const User = require('../../models/user');
+// const User = require('../../models/user');
 const { ctrlWrapper } = require('../../decorators');
-const { cloudinary, HttpError } = require('../../utils');
+// const { cloudinary, HttpError } = require('../../utils');
 
 const updateAvatar = ctrlWrapper(async (req, res) => {
-  const { avatarId } = req.user;
-  if (avatarId) await cloudinary.destroy(avatarId);
+  console.log('req.file:', req.file);
+  // const { avatarId } = req.user;
+  // if (avatarId) await cloudinary.destroy(avatarId);
 
-  console.log('req.file.path: ', req.file.path);
-  // eslint-disable-next-line camelcase
-  const { url, public_id } = await cloudinary.upload(req.file.path);
-  const avatar = { avatarUrl: url, avatarId: public_id };
-  const newUser = await User.findByIdAndUpdate(req.user._id, avatar);
-  if (!newUser) throw HttpError(404);
-  res.status(200).json({ avatar: url });
+  // console.log('req.file.path: ', req.file.path);
+  // // eslint-disable-next-line camelcase
+  // const { url, public_id } = await cloudinary.upload(req.file.path);
+  // const avatar = { avatarUrl: url, avatarId: public_id };
+  // const newUser = await User.findByIdAndUpdate(req.user._id, avatar);
+  // if (!newUser) throw HttpError(404);
+  // res.status(200).json({ avatar: url });
 });
 
 module.exports = updateAvatar;
